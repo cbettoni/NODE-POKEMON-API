e7154381fd6fc66c07c04dbb5cdd99d95ed1fc32
Christelle <christelle.bettoni@gmail.com>
2025-07-31 16:54:02 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+let pokemons = require('./mock-pokemon');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => res.send('Hello again, Express!'))
+
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}`)
+})
+
+//nb total de pokémons
+app.get('/api/pokemons', (req,res) => {
+    res.send(`Il y a ${pokemons. length} pokémons dans le pokédex pour le moment.`)
+})
+
+
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
